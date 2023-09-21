50daae6a019f824d1e748cf3f724b3c91d733bb8
jepiosbu <jepiosbu@gmail.com>
2023-09-21 07:04:18 -0500

    Version3.3

---
+++ PlantillaBrasil/Splahs.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace PlantillaBrasil
+{
+    partial class Splahs
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splahs));
+            this.Reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            this.TimeOpen = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Reproductor
+            // 
+            this.Reproductor.Enabled = true;
+            this.Reproductor.Location = new System.Drawing.Point(0, -1);
+            this.Reproductor.Name = "Reproductor";
+            this.Reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Reproductor.OcxState")));
+            this.Reproductor.Size = new System.Drawing.Size(646, 364);
+            this.Reproductor.TabIndex = 0;
+            this.Reproductor.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.Reproductor_PlayStateChange);
+            this.Reproductor.Enter += new System.EventHandler(this.Reproductor_Enter);
+            // 
+            // TimeOpen
+            // 
+            this.TimeOpen.Interval = 20000;
+            this.TimeOpen.Tick += new System.EventHandler(this.TimeOpen_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(703, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 46);
+            this.label1.TabIndex = 1;
+            // 
+            // Splahs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 434);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Reproductor);
+            this.Name = "Splahs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splahs";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Reproductor;
+        private System.Windows.Forms.Timer TimeOpen;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PlantillaBrasil/Splahs.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PlantillaBrasil
+{
+    public partial class Splahs : Form
+    {
+        string[] vectorRutasVideo = new string[3];
+        int con1;
+        int con2;
+        int sum;
+        int value;
+        public Splahs()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            vectorRutasVideo[0] = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Kimetsu.mp4");
+            File.WriteAllBytes(vectorRutasVideo[0], Properties.Resources.Kimetsu);
+            vectorRutasVideo[1] = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Solo.mp4");
+            File.WriteAllBytes(vectorRutasVideo[1], Properties.Resources.Solo);
+            vectorRutasVideo[2] = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Tokyo.mp4");
+            File.WriteAllBytes(vectorRutasVideo[2], Properties.Resources.Tokyo);
+
+            this.Size = SystemInformation.PrimaryMonitorSize;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+        }
+        private void Reproductor_Enter(object sender, EventArgs e)
+        {
+            con1 = Properties.Settings.Default.RamdonVideo1;
+            con2 = Properties.Settings.Default.RamdonVideo2;
+            Random random = new Random();
+            value= random.Next(0, 3);
+            while (con1 == value || con2 == value)
+            {
+                value = random.Next(0, 3);
+            }
+            Reproductor.URL = vectorRutasVideo[value]; 
+            Reproductor.Ctlcontrols.play();
+            TimeOpen.Enabled = true;
+            Reproductor.Ctlenabled = false;
+            Reproductor.Size = SystemInformation.PrimaryMonitorSize;
+        }
+
+        private void Reproductor_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            
+            if (Reproductor.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                Reproductor.fullScreen = true;
+            }
+        }
+
+        private void TimeOpen_Tick(object sender, EventArgs e)
+        {
+            sum++;
+            if (sum == 0)
+            {
+                Properties.Settings.Default.RamdonVideo1 = value;
+                Properties.Settings.Default.Suma = sum;
+            }
+            if (sum == 1)
+            {
+                Properties.Settings.Default.RamdonVideo2=value;
+                Properties.Settings.Default.Suma = sum;
+            }
+            if (sum == 2)
+            {
+                Properties.Settings.Default.RamdonVideo1 = value;
+                Properties.Settings.Default.Suma = 0;
+
+            }
+            Reproductor.Ctlcontrols.stop();
+            Properties.Settings.Default.RamdonVideo1=value;
+            Properties.Settings.Default.Save();
+            Bienvenida form2 = new Bienvenida();
+            TimeOpen.Enabled = false;
+            this.Hide();
+            form2.Show();
+        }
+
+    }
+}
