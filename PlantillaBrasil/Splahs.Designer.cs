9ad704bcd6b943ea6a95cad2d3ead064eada5234
jepiosbu <jepiosbu@gmail.com>
2023-09-20 19:30:08 -0500

    Version1.1

---
+++ PlantillaBrasil/Splahs.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace PlantillaBrasil
+{
+    partial class Splahs
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splahs));
+            this.Reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            this.TimeOpen = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Reproductor
+            // 
+            this.Reproductor.Enabled = true;
+            this.Reproductor.Location = new System.Drawing.Point(0, -1);
+            this.Reproductor.Name = "Reproductor";
+            this.Reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Reproductor.OcxState")));
+            this.Reproductor.Size = new System.Drawing.Size(646, 364);
+            this.Reproductor.TabIndex = 0;
+            this.Reproductor.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.Reproductor_PlayStateChange);
+            this.Reproductor.Enter += new System.EventHandler(this.Reproductor_Enter);
+            // 
+            // TimeOpen
+            // 
+            this.TimeOpen.Interval = 20000;
+            this.TimeOpen.Tick += new System.EventHandler(this.TimeOpen_Tick);
+            // 
+            // Splahs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 434);
+            this.Controls.Add(this.Reproductor);
+            this.Name = "Splahs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splahs";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Splahs_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Reproductor;
+        private System.Windows.Forms.Timer TimeOpen;
+    }
+}
+
---
+++ PlantillaBrasil/Splahs.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+
+namespace PlantillaBrasil
+{
+    public partial class Splahs : Form
+    {
+        int con;
+        string[] vectorRutasVideo = new string[3];
+        public Splahs()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = SystemInformation.PrimaryMonitorSize;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            vectorRutasVideo[0] = @"C:\Users\ender\Downloads\videoplayback.mp4";
+            vectorRutasVideo[1] = @"C:\Users\ender\Downloads\videoplayKimetsu.mp4";
+            vectorRutasVideo[2] = @"C:\Users\ender\Downloads\videoplayGojoSatoru.mp4";
+            con = Properties.Settings.Default.RamdonVideo;
+            
+
+
+}
+        private void Reproductor_Enter(object sender, EventArgs e)
+        {
+            string mp4Path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "videoplayback.mp4");
+
+            if (!File.Exists(mp4Path))
+            {
+                File.WriteAllBytes(mp4Path, Properties.Resources.videoplayback);
+            }
+            Reproductor.URL = mp4Path;
+            Reproductor.Ctlcontrols.play();
+            TimeOpen.Enabled = true;
+            Reproductor.Ctlenabled = false;
+            Reproductor.Size = SystemInformation.PrimaryMonitorSize;
+        }
+
+        private void Reproductor_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            
+            if (Reproductor.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                Reproductor.fullScreen = true;
+            }
+        }
+
+        private void TimeOpen_Tick(object sender, EventArgs e)
+        {
+            Bienvenida form2 = new Bienvenida();
+            TimeOpen.Enabled = false;
+            this.Hide();
+            form2.Show();
+            
+            
+        }
+
+        private void Splahs_FormClosed(object sender, FormClosedEventArgs e)
+        {
+       
+        }
+    }
+}
